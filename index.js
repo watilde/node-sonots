6c9eb29a90457bfd1980fff167eca6b286247989
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-04-23 09:09:53 +0900

    Update name

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+#!/usr/bin/env node
 var pictureTube = require('picture-tube')
 var tube = pictureTube();
 tube.pipe(process.stdout);
 
 var fs = require('fs');
-fs.createReadStream('img/sonots-san.png').pipe(tube);
+fs.createReadStream('img/sonots.png').pipe(tube);
 
 module.exports = function (cb) {
   tube.pipe(cb);
